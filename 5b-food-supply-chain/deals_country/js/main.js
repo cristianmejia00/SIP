c117886bc897dcbd6cc0b8619941ce45d0f0ec60
cristianmejia00 <cristianmejia00@hotmail.com>
2019-11-07 12:02:39 +0900

    graying out darker

--- 5b-food-supply-chain/deals_country/js/main.js
+++ 5b-food-supply-chain/deals_country/js/main.js
@@ -631,7 +631,8 @@ function nodeActive(a) {
 
 // This is the function that hides or shows the nodes of a cluster.
 function showCluster(a) {
-    var greyColor = '#E0E0E0';
+    //var greyColor = '#E0E0E0';
+    var greyColor = '#D5D5D5';
     var b = sigInst.clusters[a];
     if (b && 0 < b.length) {
         showGroups(false);
